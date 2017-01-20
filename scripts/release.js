8bc17f55c390c62f4eefcbc02d8ad40d79121a6b
Chris Thielen <github@sandgnat.com>
2017-01-20 13:24:07 -0600

    chore(*): update release.js script

--- scripts/release.js
+++ scripts/release.js
@@ -1,12 +1,16 @@
 #!env node
 "use strict";
 
+let version = require('../package.json').version;
+
 require('shelljs/global');
 let readlineSync = require('readline-sync');
+let fs = require('fs');
+let path = require('path');
 let util = require('./util');
 let _exec = util._exec;
 
-let version = require('../package.json').version;
+cd(path.join(__dirname, '..'));
 
 if (!readlineSync.keyInYN('Did you bump the version number in package.json?')) {
   process.exit(1);
@@ -16,12 +20,24 @@ if (!readlineSync.keyInYN('Did you update CHANGELOG.md using scripts/update_chan
   process.exit(1);
 }
 
-if (!readlineSync.keyInYN('Did you commit/push all changes back to origin?')) {
+if (!readlineSync.keyInYN('Did you push all changes back to origin?')) {
+  process.exit(1);
+}
+
+if (!readlineSync.keyInYN('Ready to publish?')) {
   process.exit(1);
 }
 
 util.ensureCleanMaster('master');
+
+_exec('npm run package');
+_exec(`npm run docs`);
+
+// publish to npm first
 _exec(`npm publish`);
+
+// then tag and push tag
 _exec(`git tag ${version}`);
 _exec(`git push origin ${version}`);
 
+console.log("\n\nAPI docs generated (but not deployed) at ./_docs");
