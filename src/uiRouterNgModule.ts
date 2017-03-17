29f2d0e05e1cfddcfc4210c4222b7cc8dfe68bc8
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2017-03-17 10:42:20 +0000

    upgrade to angular 4.

--- src/uiRouterNgModule.ts
+++ src/uiRouterNgModule.ts
@@ -10,7 +10,7 @@ import { UIView } from "./directives/uiView";
 import { UrlRuleHandlerFn, TargetState, TargetStateDef, UIRouter } from "@uirouter/core";
 import { _UIROUTER_INSTANCE_PROVIDERS, _UIROUTER_SERVICE_PROVIDERS } from "./providers";
 
-import { ROUTES } from "@angular/router/src/router_config_loader";
+import { ROUTES } from "@angular/router";
 /** @hidden */ export const UIROUTER_ROOT_MODULE  = new OpaqueToken("UIRouter Root Module");
 /** @hidden */ export const UIROUTER_MODULE_TOKEN = new OpaqueToken("UIRouter Module");
 /** @hidden */ export const UIROUTER_STATES       = new OpaqueToken("UIRouter States");
