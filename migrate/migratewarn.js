dad26af778e06fbdfd8588e928e2ba51450506d6
Chris Thielen <cthielen@netflix.com>
2017-04-23 12:26:19 -0700

    chore(migrate): Print blog post link when warning about package migration

--- migrate/migratewarn.js
+++ migrate/migratewarn.js
@@ -6,5 +6,7 @@ var migrate = require('./migrate.json');
 
 if (pkg.name === migrate.old) {
   var warning = 'WARNING! this npm package "' + migrate.old + '" has moved to "' + migrate.new + '".  Please update your package.json';
+  var warning2 = 'WARNING! See https://ui-router.github.io/blog/uirouter-scoped-packages/ for details';;
   console.log("\x1b[1m\x1b[37m\x1b[41m%s\x1b[0m", warning);
+  console.log("\x1b[1m\x1b[37m\x1b[41m%s\x1b[0m", warning2);
 }
