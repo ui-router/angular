c688f5b6cae821bb4ee2be9de3d385ddb3fd360c
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2017-06-14 12:24:19 +0000

    [ssr][lazyload] prevent page flicker

--- src/uiRouterNgModule.ts
+++ src/uiRouterNgModule.ts
@@ -2,12 +2,12 @@
 /** */
 import { Ng2StateDeclaration } from "./interface";
 import {
-  NgModule, ModuleWithProviders, ANALYZE_FOR_ENTRY_COMPONENTS, Provider, Injector, InjectionToken
+  NgModule, ModuleWithProviders, ANALYZE_FOR_ENTRY_COMPONENTS, Provider, Injector, InjectionToken, APP_INITIALIZER, PLATFORM_ID,
 } from "@angular/core";
-import { CommonModule, LocationStrategy, HashLocationStrategy, PathLocationStrategy } from "@angular/common";
+import { CommonModule, LocationStrategy, HashLocationStrategy, PathLocationStrategy, isPlatformServer } from "@angular/common";
 import { _UIROUTER_DIRECTIVES } from "./directives/directives";
 import { UIView } from "./directives/uiView";
-import { UrlRuleHandlerFn, TargetState, TargetStateDef, UIRouter } from "@uirouter/core";
+import { UrlRuleHandlerFn, TargetState, TargetStateDef, UIRouter, TransitionService } from "@uirouter/core";
 import { _UIROUTER_INSTANCE_PROVIDERS, _UIROUTER_SERVICE_PROVIDERS } from "./providers";
 
 import { ROUTES } from "@angular/router";
@@ -16,12 +16,24 @@ import { ROUTES } from "@angular/router";
 /** @hidden */ export const UIROUTER_STATES       = new InjectionToken("UIRouter States");
 // /** @hidden */ export const ROUTES = UIROUTER_STATES;
 
+export function onTransitionReady(transitionService: TransitionService, plateformId) {
+    if (isPlatformServer(plateformId)) {
+       return () => Promise.resolve();
+    }
+
+    return () => new Promise(resolve => {
+      transitionService.onSuccess({}, resolve);
+      transitionService.onError({}, resolve);
+    });
+}
+
 export function makeRootProviders(module: StatesModule): Provider[] {
     return [
         { provide: UIROUTER_ROOT_MODULE,         useValue: module,              multi: true},
         { provide: UIROUTER_MODULE_TOKEN,        useValue: module,              multi: true },
         { provide: ROUTES,                       useValue: module.states || [], multi: true },
         { provide: ANALYZE_FOR_ENTRY_COMPONENTS, useValue: module.states || [], multi: true },
+        { provide: APP_INITIALIZER, useFactory: onTransitionReady, deps: [TransitionService, PLATFORM_ID], multi: true },
     ];
 }
 
