992847a452188ca1a819938348a5f32af5054e53
Chris Thielen <cthielen@netflix.com>
2020-07-05 19:46:12 -0700

    test(uiSref): Make uiSref tests easier to read

--- test/uiSref/uiSref.spec.ts
+++ test/uiSref/uiSref.spec.ts
@@ -1,11 +1,12 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { Component, DebugElement, ViewChildren, QueryList } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
 import { UIRouterModule } from '../../src/uiRouterNgModule';
 import { UISref } from '../../src/directives/uiSref';
-import { UIRouter, TargetState, TransitionOptions, StateService } from '@uirouter/core';
-import { Subject, Subscription } from 'rxjs';
+import { UIRouter, TargetState, TransitionOptions } from '@uirouter/core';
+import { Subscription } from 'rxjs';
 import { clickOnElement } from '../testUtils';
 
 describe('uiSref', () => {
@@ -41,138 +42,115 @@ describe('uiSref', () => {
     }
   }
 
+  const setup = () => {
+    const fixture = TestBed.configureTestingModule({
+      declarations: [TestComponent],
+      imports: [UIRouterModule.forRoot({ useHash: true })],
+      providers: [{ provide: APP_BASE_HREF, useValue: '/root' }],
+    }).createComponent(TestComponent);
+    fixture.detectChanges();
+    const srefElements = fixture.debugElement.queryAll(By.directive(UISref));
+    const router = fixture.debugElement.injector.get(UIRouter);
+
+    return { fixture, srefElements, router };
+  };
+
+  // Extract the logical portion of the URL after the hash
+  const urlOfElement = (srefElement: DebugElement) => srefElement.nativeElement.href.replace(/^[^#]*#/, '');
+
+  it('renders an href for a state with an url', () => {
+    const { fixture, srefElements, router } = setup();
+    router.stateRegistry.register({ name: 'urlstate', url: '/myurl' });
+    fixture.componentInstance.linkA = 'urlstate';
+    fixture.detectChanges();
+    expect(srefElements[0].nativeElement.hasAttribute('href')).toBeTruthy();
+    expect(urlOfElement(srefElements[0])).toBe('/myurl');
+  });
+
+  // it('renders an empty href for a url-less state', () => {
+  //   const { fixture, srefElements, router } = setup();
+  //   router.stateRegistry.register({ name: 'urlless' });
+  //   fixture.componentInstance.linkA = 'urlless';
+  //   fixture.detectChanges();
+  //   expect(srefElements[0].nativeElement.hasAttribute('href')).toBeTruthy();
+  //   expect(urlOfElement(srefElements[0])).toBe('');
+  // });
+
+  it('renders no href when the sref state is empty', () => {
+    const { fixture, srefElements } = setup();
+    fixture.componentInstance.linkA = null;
+    expect(srefElements[0].nativeElement.hasAttribute('href')).toBeFalsy();
+  });
+
+  it('should ignore the click event when the sref state is empty', () => {
+    const { fixture, srefElements, router } = setup();
+    const gospy = jest.spyOn(router.stateService, 'go');
+    fixture.componentInstance.linkA = null;
+    fixture.componentInstance.linkB = null;
+    fixture.detectChanges();
+
+    clickOnElement(srefElements[0]);
+    expect(gospy).not.toHaveBeenCalled();
+
+    clickOnElement(srefElements[0]);
+    expect(gospy).not.toHaveBeenCalled();
+  });
+
+  it('should call stateService.go with the linked state when clicked', () => {
+    const { fixture, srefElements, router } = setup();
+    const gospy = jest.spyOn(router.stateService, 'go');
+    fixture.componentInstance.linkA = 'stateref';
+    fixture.detectChanges();
+    clickOnElement(srefElements[0]);
+    expect(gospy).toHaveBeenCalled();
+    expect(gospy.mock.calls[0][0]).toBe('stateref');
+  });
+
+  it('should ignore the click event when target is _blank', () => {
+    const { fixture, srefElements, router } = setup();
+    const gospy = jest.spyOn(router.stateService, 'go');
+    router.stateRegistry.register({ name: 'statea', url: '/statea' });
+    fixture.componentInstance.linkA = 'statea';
+    fixture.componentInstance.targetA = '_blank';
+    fixture.detectChanges();
+    clickOnElement(srefElements[0]);
+    expect(gospy).not.toHaveBeenCalled();
+  });
+
+  describe('opening in a new tab', () => {
+    let srefElement: DebugElement;
+    let gospy: jest.SpyInstance;
+
+    beforeEach(() => {
+      const { fixture, srefElements, router } = setup();
+      srefElement = srefElements[0];
+      gospy = jest.spyOn(router.stateService, 'go');
+      fixture.componentInstance.targetA = 'somestate';
+      fixture.detectChanges();
+    });
+
+    it('should fallback to the browser default when the button is not the left', () => {
+      clickOnElement(srefElement, 1);
+      expect(gospy).not.toHaveBeenCalled();
+    });
+
+    it('should fallback to the browser default when the button is not set', () => {
+      clickOnElement(srefElement, null);
+      expect(gospy).not.toHaveBeenCalled();
+    });
+
+    it('should fallback to the browser default when the meta key is pressed', () => {
+      clickOnElement(srefElement, 0, true);
+      expect(gospy).not.toHaveBeenCalled();
+    });
+
+    it('should fallback to the browser default when the ctrl key is pressed', () => {
+      clickOnElement(srefElement, 0, false, true);
+      expect(gospy).not.toHaveBeenCalled();
+    });
+  });
+
   describe('when applied to a link tag', () => {
-    describe('when the uiSref is empty', () => {
-      let des: DebugElement[];
-      let fixture: ComponentFixture<TestComponent>;
-      let gospy: jasmine.Spy;
-
-      beforeEach(() => {
-        fixture = TestBed.configureTestingModule({
-          declarations: [TestComponent],
-          imports: [UIRouterModule.forRoot({ useHash: true })],
-        }).createComponent(TestComponent);
-        fixture.detectChanges();
-        des = fixture.debugElement.queryAll(By.directive(UISref));
-        const stateService = fixture.debugElement.injector.get(StateService);
-        gospy = spyOn(stateService, 'go');
-      });
-
-      it('should not bind "null" string to `href`', () => {
-        expect(des[0].nativeElement.hasAttribute('href')).toBeFalsy();
-        expect(des[1].nativeElement.hasAttribute('href')).toBeFalsy();
-      });
-
-      it('should ignore the click event', () => {
-        clickOnElement(des[0]);
-        expect(gospy).not.toHaveBeenCalled();
-
-        clickOnElement(des[1]);
-        expect(gospy).not.toHaveBeenCalled();
-      });
-    });
-
-    describe('when the uiSref is not empty', () => {
-      let des: DebugElement[];
-      let comp: TestComponent;
-      let uiRouterMock: UIRouter;
-      let fixture: ComponentFixture<TestComponent>;
-
-      beforeEach(async(() => {
-        uiRouterMock = {
-          globals: {
-            states$: new Subject(),
-          },
-          stateService: {
-            go: jest.fn(),
-            target: jest.fn(),
-            href: jest.fn(),
-          },
-        } as any;
-        TestBed.configureTestingModule({
-          declarations: [TestComponent],
-          imports: [UIRouterModule.forRoot({ useHash: true })],
-        })
-          .overrideComponent(TestComponent, {
-            set: {
-              providers: [{ provide: UIRouter, useValue: uiRouterMock }],
-            },
-          })
-          .compileComponents();
-      }));
-
-      beforeEach(() => {
-        fixture = TestBed.createComponent(TestComponent);
-
-        comp = fixture.componentInstance;
-        comp.linkA = '';
-        fixture.detectChanges();
-        des = fixture.debugElement.queryAll(By.directive(UISref));
-      });
-
-      describe('when target is _blank', () => {
-        beforeEach(() => {
-          comp.targetA = '_blank';
-          fixture.detectChanges();
-        });
-
-        describe('when clicked', () => {
-          beforeEach(() => {
-            clickOnElement(des[0]);
-          });
-
-          it('should ignore the click event', () => {
-            expect(uiRouterMock.stateService.go).not.toHaveBeenCalled();
-          });
-        });
-      });
-
-      describe('when target is not _blank', () => {
-        beforeEach(() => {
-          comp.targetA = '';
-          comp.linkA = 'state';
-          fixture.detectChanges();
-        });
-
-        describe('when clicked', () => {
-          beforeEach(() => {
-            clickOnElement(des[0]);
-          });
-
-          it('should navigate to the state', () => {
-            expect(uiRouterMock.stateService.go).toHaveBeenCalled();
-          });
-        });
-      });
-
-      describe('opening in a new tab', () => {
-        beforeEach(() => {
-          comp.targetA = '';
-          fixture.detectChanges();
-        });
-
-        it('should fallback to the browser default when the button is not the left', () => {
-          clickOnElement(des[0], 1);
-          expect(uiRouterMock.stateService.go).not.toHaveBeenCalled();
-        });
-
-        it('should fallback to the browser default when the button is not set', () => {
-          clickOnElement(des[0], null);
-          expect(uiRouterMock.stateService.go).not.toHaveBeenCalled();
-        });
-
-        it('should fallback to the browser default when the meta key is pressed', () => {
-          clickOnElement(des[0], 0, true);
-          expect(uiRouterMock.stateService.go).not.toHaveBeenCalled();
-        });
-
-        it('should fallback to the browser default when the ctrl key is pressed', () => {
-          clickOnElement(des[0], 0, false, true);
-          expect(uiRouterMock.stateService.go).not.toHaveBeenCalled();
-        });
-      });
-    });
-
     describe('when the bound values change', () => {
       let fixture: ComponentFixture<TestComponent>;
       let comp: TestComponent;
