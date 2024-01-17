55e94bf82dc78608576a84ff51cd3ac5b89b2ff7
Rolando <rolando@everymundo.com>
2024-01-17 00:22:09 -0500

    Updated v17 project to properly bootstrap the AppModules instead of the AppComponent.

--- test-angular-versions/v17/src/main.ts
+++ test-angular-versions/v17/src/main.ts
@@ -1,6 +1,5 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent, appConfig)
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch((err) => console.error(err));
