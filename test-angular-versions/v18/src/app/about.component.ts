47889aa8332c8507862bab36a11969b6aa703c08
lukaVarga <varga.luka93@gmail.com>
2024-07-10 08:49:43 +0200

    chore: Adjustments following code review

--- test-angular-versions/v18/src/app/about.component.ts
+++ test-angular-versions/v18/src/app/about.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   template: ` <p>about works!</p> `,
+  standalone: true,
 })
 export class AboutComponent {}
--- test-angular-versions/v18/src/app/home.component.ts
+++ test-angular-versions/v18/src/app/home.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: ` <p>home works!</p> `,
+  standalone: true,
 })
 export class HomeComponent {}
